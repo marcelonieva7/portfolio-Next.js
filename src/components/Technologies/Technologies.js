b5ec2b4e78bae4e16b83fbdaaee9dc961e4424ae
marcelonieva7 <marcelonieva7@gmail.com>
2021-09-09 23:52:56 -0300

    set tech section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,49 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiFirebase, DiReact, DiAndroid } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+    <SectionDivider/>
+    <SectionTitle main>Tecnologías</SectionTitle>
+    <SectionText>
+      Tecnologias que domino desde el back hasta el diseño del front.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem"/>
+        <ListContainer>
+          <ListTitle>Fornt-End</ListTitle>
+          <ListParagraph>
+            Experiencia con <br />
+            React.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem"/>
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experiencia con <br />
+            Node.js y Mongo DB
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiAndroid size="3rem"/>
+        <ListContainer>
+          <ListTitle>Mobile</ListTitle>
+          <ListParagraph>
+            Experiencia con <br />
+            React Native
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+
+  </Section>
 );
 
 export default Technologies;
