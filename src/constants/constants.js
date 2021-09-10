3cf5953e5a510d7741f742da060d4c65021fa295
marcelonieva7 <marcelonieva7@gmail.com>
2021-09-10 00:30:56 -0300

    set about me section

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -38,9 +38,7 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: 1993, text: 'Aterrizaje en la Tierra', },
+  { year: 2020, text: 'Comienzo a estudiar desarrollo web', },
+  { year: 2021, text: 'Desarrollo mis primeros proyectos y empiezo como freelancer', },
 ];
