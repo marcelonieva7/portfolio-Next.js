47ff63c7e45421663e873228fdb159d5c2ce786f
Marcelo Nieva <marcelonieva7@gmail.com>
2022-07-20 12:42:28 -0300

    reduce time in hero animation

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,7 +5,7 @@ import { LeftSection } from './HeroStyles';
 
 const Hero = ({title}) => {
   const variants = {
-    visible: { opacity: 1, x: 0, transition: { duration: 1.2, delay: 0.85 } },
+    visible: { opacity: 1, x: 0, transition: { duration: 1.1, delay: 0.45 } },
     hidden: { opacity: 0, x: -75 }
   };
   return (
